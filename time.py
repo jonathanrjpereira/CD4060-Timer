e3ee1e12f083b0d362139221d94c5369e282283b
jonathanrjpereira <jonathanrjpereira@gmail.com>
2019-03-19 20:47:27 +0530

    Output Timing Calculator

--- time.py
+++ time.py
@@ -1,14 +1,18 @@
 # Calculate timing of Output Qn for fixed capacitor & variable resistor.
 
-# resistor = [1000000,2.2*1000000,4.7*1000000,10*1000000]
-#             1Mohm   2.2Mohm     4.7Mohm     10Mohm
+resistor = [1000000,2.2*1000000,4.7*1000000,10*1000000]
+#            1Mohm   2.2Mohm     4.7Mohm     10Mohm
 
-# for r in resistor:
-#     C = 0.22 / 1000000 # C=0.22uF
-#     fosc = 1 / (2.5 * r * C )
-#     print(fosc)
 
-fosc = [1.81, 0.826, 0.386, 0.181]
+fosc = []
+for r in resistor:
+    R = 910000      #470K=24hr / 910K=1hr
+    C = 0.22 / 1000000 # C=0.22uF
+    f = 1 / (2.2 * (r+R) * C )
+    fosc.append(f)
+print ("Fosc = " + str(fosc))
+
+# fosc = [1.81, 0.826, 0.386, 0.181]
 
 for freq in fosc:
     print("\nFrequency = " + str(freq) + str("Hz") )
